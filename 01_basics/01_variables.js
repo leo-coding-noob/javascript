e291e55014480f91b1c9c20d554acece207e3931
leo-coding-noob <leosaurav9@gmail.com>
2024-09-15 12:24:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = "12321"
+let accountEmail ="saurava2gmail.com"
+var accountPassword= "123445"
+accountCity = "bkt"
+let accountState;
+
+// accountId = 2
+
+/*prefer not to use var cause of lack of functional scope{}*/
+accountEmail = "hc@hc.com"
+accountPassword = "34567"
+accountCity = "bhaktapur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]   )
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+
+console.log("saurav")
+
+let name="saurav" //String
+let age=18 //numbervalue
+let isLoggedIn= false //Boolean
+
+// number => 2 to power 53
+// bigint( large number)
+//string
+//boolean
+//null -> standalone value(represnetation of empty value)
+//undefined => declared varible but not input data
+// symbol ->helps to find uniqueness
+
+//object
+
+console.log(typeof undefined);
